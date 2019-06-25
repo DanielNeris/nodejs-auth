92aa786bac40dca8da6642f58051ee3a724e364d
Daniel de Souza Neri <danielneris01@gmail.com>
2019-06-25 16:59:53 -0300

    gerando token de auth

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const authConfig = require('../config/auth');
 
 const User = require('../models/user');
 
@@ -18,6 +21,29 @@ router.post('/register', async (req, res) => {
         return res.send({ user });
     } catch (error) {
         res.status(400).send({ error });
+    }
+});
+
+router.post('/authenticate', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email }).select('+password');
+
+        if(!user)
+            return res.status(400).send({'massage': 'User not found'});
+            
+        if(!await bcrypt.compare(password, user.password))
+            return res.status(400).send({'message': 'Invalid password'});
+
+        user.password = undefined;
+
+        const token = jwt.sign({ id: user._id }, authConfig.secret, {
+            expiresIn: 86400,
+        });
+
+        return res.send({ user, token });
+    } catch (error) {
+        return res.status(400).send({ error });
     }
 });
 
